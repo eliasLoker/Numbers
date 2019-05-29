0dba986d13edc9a91da748ea44c7f17b0d5dc189
Alexandr <alexandr.agress1on@gmail.com>
2019-05-29 16:42:09 +0300

    - добавил NavigationDrawer

--- app/src/main/java/com/example/myplaceinfo/MainActivity.kt
+++ app/src/main/java/com/example/myplaceinfo/MainActivity.kt
@@ -1,20 +1,53 @@
 package com.example.myplaceinfo
 
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import com.example.my.ContainerFragment
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawer: DrawerLayout
+    private lateinit var navigationView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.nd_activity_main_for_start_view)
 
-        val fragment = supportFragmentManager.findFragmentById(R.id.fragment_activity_container)
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        drawer = findViewById(R.id.drawer_layout)
+
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+
+
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.container_for_fragments)
         if (fragment == null) {
                 supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_activity_container, ContainerFragment.newInstance())
+                .add(R.id.container_for_fragments, ContainerFragment.newInstance())
                 .commit()
         }
+
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
